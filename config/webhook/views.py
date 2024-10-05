edf485ea6ded089e59a36b5a5147348643c0cccd
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-05 12:17:06 +0500

    send Coordinates instead of address

--- config/webhook/views.py
+++ config/webhook/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpRequest
 from rest_framework.views import APIView
 from rest_framework.request import Request
+from rest_framework.permissions import AllowAny
 
 
 class IikoOrderUpdateAPIView(APIView):
+    authentication_classes = []  # Disable authentication
+    permission_classes = [AllowAny]  # Allow any user to access this view
 
     def post(self, request: HttpRequest | Request):
-
         print("Headers", request.headers)
 
         data = request.data
