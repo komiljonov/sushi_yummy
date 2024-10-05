32f698ca29484599d3f18dab27e62337fa11adac
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-05 16:25:39 +0500

    dfg

--- config/config/settings.py
+++ config/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "restframework",
+    "rest_framework",
     "corsheaders",
     "common",
     "bot",
