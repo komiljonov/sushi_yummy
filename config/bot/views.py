a555ee91a835f35d2b63c95427e233b0b3b6677e
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-08 13:19:11 +0500

    fdg

--- config/bot/views.py
+++ config/bot/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from django.db.models import Q
 
 from bot.models import User
 from data.users.serializers import UserSerializer
+
 # Create your views here.
 
 
-
-
 class SearchUsersAPIView(ListAPIView):
-    
+
     serializer_class = UserSerializer
-    
+
     def get_queryset(self):
         queryset = User.objects.all()
-        
-        if q := self.kwargs.get('search'):
-            queryset = queryset.filter(
-                Q(name__icontains=q) | Q(number__icontains=q)
-            )
-        
+
+        if q := self.kwargs.get("search"):
+            queryset = queryset.filter(Q(name__icontains=q) | Q(number__icontains=q))
+
         return queryset
