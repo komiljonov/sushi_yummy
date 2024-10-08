3e2e5b4d5706a4542249eda3bb0fa12863596551
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-08 13:18:58 +0500

    fdg

---
+++ config/bot/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import SearchUsersAPIView
+
+
+urlpatterns = [
+    path("", SearchUsersAPIView.as_view()),
+    path("<str:search>", SearchUsersAPIView.as_view())
+]
