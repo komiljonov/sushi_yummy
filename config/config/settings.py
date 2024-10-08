d9aabb357570169572d417b704180d9225caa674
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-08 11:43:56 +0500

    fdg

--- config/config/settings.py
+++ config/config/settings.py
@@ -57,21 +57,23 @@ INSTALLED_APPS = [
     "data.referral",
     "data.analytics",
     "data.sync",
-    "data.taxi"
+    "data.taxi",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://127.0.01:3000",
-    "https://sushi-yummy.vercel.app",
-    "http://localhost:63342",
-    "https://api.sushi.sectorsoft.uz",
-    "http://api.sushi.sectorsoft.uz"
-]
+# CORS_ALLOWED_ORIGINS = [
+    # "http://localhost:3000",
+    # "https://127.0.01:3000",
+    # "https://sushi-yummy.vercel.app",
+    # "http://localhost:63342",
+    # "https://api.sushi.sectorsoft.uz",
+    # "http://api.sushi.sectorsoft.uz",
+# ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://api.sushi.sectorsoft.uz',  # Add your API domain here
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+#CSRF_TRUSTED_ORIGINS = [
+#   "https://api.sushi.sectorsoft.uz",
+#]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
