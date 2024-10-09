2b7cbb2b55603a775cc98f0ab4563a303ee3ef5d
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-09 21:34:08 +0500

    dfg

--- config/data/cart/views.py
+++ config/data/cart/views.py
@@ -11,6 +11,7 @@ from rest_framework.generics import CreateAPIView
 from data.taxi.serializers import TaxiSerializer
 from utils.millenium import Millenium
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
