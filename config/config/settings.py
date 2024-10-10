29e9c5041706a6fec1c96aba9a0773592fcd6996
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-10 15:06:02 +0500

    gdfg

--- config/config/settings.py
+++ config/config/settings.py
@@ -168,8 +168,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=48),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
