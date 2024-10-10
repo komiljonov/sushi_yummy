ecdc389b17fd51e5461065ef9baf1f435449bb3c
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-10-10 13:26:25 +0500

    dfg

--- config/data/product/serialisers.py
+++ config/data/product/serialisers.py
@@ -31,14 +31,14 @@ class ProductSerializer(ModelSerializer):
 
     def get_sale_count(self, obj: Product):
         # return 5
-        # return obj.cart_items.filter(cart__status__in=[
-        #     "PENDING_PAYMENT",
-        #     "PENDING",
-        #     "PENDING_KITCHEN",
-        #     "PREPARING",
-        #     "DELIVERING",
-        #     "DONE",
-        # ]).count()
+        return obj.cart_items.filter(cart__status__in=[
+            "PENDING_PAYMENT",
+            "PENDING",
+            "PENDING_KITCHEN",
+            "PREPARING",
+            "DELIVERING",
+            "DONE",
+        ]).count()
 
 
 class ProductCreateSerializer(ModelSerializer):
